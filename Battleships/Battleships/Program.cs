75960dabb9e79b607eb6360914bddeca33c1983e
alptuncs <alptuncsever@gmail.com>
2022-07-04 15:30:20 +0300

    Adding comments
    Adding comments

--- Battleships/Battleships/Program.cs
+++ Battleships/Battleships/Program.cs
@@ -13,18 +13,22 @@ namespace Battleships
         {
 
             /* initializng the board and printing it on the console */
+
             Console.WriteLine("Enter the size of the board");
+            /* using regex to enforce the input to be in acceptable format that can be parsed to int */
             Regex r = new Regex(@"^\d+x{1}\d+$");
             string userInput = Console.ReadLine();
+            /* Input check */
             while (!r.IsMatch(userInput))
             {
                 Console.WriteLine("Wrong input, please write in the format of (height)x(length) E.g. 4x6");
                 userInput = Console.ReadLine();
             }
-
+            /* initialize the board height and length based on user input */
             int boardHeight = int.Parse(userInput.Substring(0, userInput.IndexOf('x')));
             int boardlength = int.Parse(userInput.Substring(userInput.IndexOf('x') + 1));
 
+            /* initialize the board with height and length */
             string[][] board = new string[boardHeight][];
 
             for (int i = 0; i < board.Length; i++)
@@ -33,7 +37,7 @@ namespace Battleships
                 test = string.Concat(Enumerable.Repeat("[] ", boardlength));
                 board[i] = test.Split(' ');
             }
-
+            /* print the board */
             for (int i = 0; i < board.Length; i++)
             {
                 for (int j = 0; j < board[i].Length; j++)
