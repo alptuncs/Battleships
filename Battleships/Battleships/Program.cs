cb490d3c56b212dc568593f9b57e415b8ad8fce8
alptuncs <alptuncsever@gmail.com>
2022-07-04 17:47:29 +0300

    Unit test
    Added the unit test project, when the board is created a ship is randomly inserted in one of the coordinates, added lives, player can play as long as they have lives remaining, lives can be seen on the screen and is being updated in every turn

--- Battleships/Battleships/Program.cs
+++ Battleships/Battleships/Program.cs
@@ -12,14 +12,30 @@ namespace Battleships
         static void Main(string[] args)
         {
             string[][] board = CreateBoard();
+            Random rnd = new Random();
+            string shipCoordinates = rnd.Next(1, board.Length).ToString() + "," + rnd.Next(1, board[0].Length).ToString();
             PrintBoard(board);
-            Console.WriteLine("Write the coordinates for the attack");
-            string attackCoordinates = Console.ReadLine();
-            int yCoordinate = int.Parse(attackCoordinates.Substring(0, attackCoordinates.IndexOf(',')));
-            int xCoordinate = int.Parse(attackCoordinates.Substring(attackCoordinates.IndexOf(',') + 1));
-            Console.Clear();
-            board[xCoordinate - 1][yCoordinate - 1] = "[x]";
-            PrintBoard(board);
+            int lives = (board.Length + board[0].Length) / 2;
+            bool succesfulAttempt = false;
+            while (lives != 0)
+            {
+
+                succesfulAttempt = Attack(ref board, shipCoordinates, ref lives);
+                if (succesfulAttempt == true)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You won");
+                    break;
+                }
+
+            }
+            if (lives == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("out of lives");
+            }
+
+
         }
 
         static string[][] CreateBoard()
@@ -45,10 +61,11 @@ namespace Battleships
 
             for (int i = 0; i < board.Length; i++)
             {
-                string test = "";
-                test = string.Concat(Enumerable.Repeat("[ ]*", boardlength));
-                board[i] = test.Split('*');
+                string row = "";
+                row = string.Concat(Enumerable.Repeat("[ ]*", boardlength));
+                board[i] = row.Split('*');
             }
+            Console.Clear();
             return board;
         }
 
@@ -63,6 +80,49 @@ namespace Battleships
                     Console.Write(board[i][j] + " ");
                 }
                 Console.Write("\n");
+            }
+        }
+
+        static bool Attack(ref string[][] board, string shipCoordinates, ref int lives)
+        {
+            Regex y = new Regex(@"\d+,{1}\d+");
+
+            Console.WriteLine("Write the coordinates for the attack");
+            string attackCoordinates = Console.ReadLine();
+
+            while (!y.IsMatch(attackCoordinates))
+            {
+                Console.WriteLine("Wrong input format, please enter the coordinates in the format (x),(y). E.g. 3,4");
+                attackCoordinates = Console.ReadLine();
+            }
+
+            int yCoordinate = int.Parse(attackCoordinates.Substring(0, attackCoordinates.IndexOf(',')));
+            int xCoordinate = int.Parse(attackCoordinates.Substring(attackCoordinates.IndexOf(',') + 1));
+
+            while (yCoordinate > board.Length || xCoordinate > board[0].Length)
+            {
+                Console.WriteLine("Coordinates can not be outside the board, enter the coordinates according the the board size");
+                attackCoordinates = Console.ReadLine();
+                yCoordinate = int.Parse(attackCoordinates.Substring(0, attackCoordinates.IndexOf(',')));
+                xCoordinate = int.Parse(attackCoordinates.Substring(attackCoordinates.IndexOf(',') + 1));
+            }
+
+            Console.Clear();
+
+            if (attackCoordinates == shipCoordinates)
+            {
+                Console.WriteLine($"                                        lives:{lives}");
+                board[xCoordinate - 1][yCoordinate - 1] = "[x]";
+                PrintBoard(board);
+                return true;
+            }
+            else
+            {
+                lives--;
+                Console.WriteLine($"                                        lives:{lives}");
+                PrintBoard(board);
+                Console.WriteLine("Missed...");
+                return false;
             }
         }
     }
