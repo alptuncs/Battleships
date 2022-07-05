8a8dfe9b258f63411fcccd74cd379e1d3de1999f
alptuncs <alptuncsever@gmail.com>
2022-07-05 17:54:39 +0300

    Target class
    Creating the target class and adding unit test

---
+++ Battleships/Battleships/Target.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships
+{
+    public class Target
+    {
+        private int xPos;
+        private int yPos;
+
+        public Target(int xPos, int yPos)
+        {
+            this.xPos = xPos;
+            this.yPos = yPos;
+        }
+
+        public int GetXPos()
+        {
+            return xPos;
+        }
+
+        public int GetYPos()
+        { 
+            return yPos;
+        }
+
+    }
+}
---
+++ Battleships/Board Tests/TargetUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using Battleships;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Board_Tests
+{
+    [TestClass]
+    public class TargetUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int expectedXPos = 0;
+            int expectedYPos = 0;
+
+            Target actualTarget = new Target(0, 0);
+
+            Assert.AreEqual(expectedYPos, actualTarget.GetYPos());
+            Assert.AreEqual(expectedXPos, actualTarget.GetXPos());
+        }
+    }
+}
