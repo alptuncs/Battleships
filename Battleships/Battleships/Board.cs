c52bb0326326d981f6de74a309d1a9ecbfc8ac56
alptuncs <alptuncsever@gmail.com>
2022-07-05 17:14:11 +0300

    Add Board Class
    Adding Board class and added unit tests for Board class functions

---
+++ Battleships/Battleships/Board.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships
+{
+    public class Board
+    {
+        private int _height;
+        private int _width;
+        private string _boardGraphicString;
+
+        public string[][] BoardSurface;
+        public string BoardGraphicString => _boardGraphicString;
+
+
+        public Board(int height, int width)
+        {
+            this._height = height;
+            this._width = width;
+
+            InitializeBoardSurface();
+        }
+
+        public void Print()
+        {
+            _boardGraphicString = "";
+
+            for (int i = 0; i < BoardSurface.Length; i++)
+            {
+                for (int j = 0; j < BoardSurface[i].Length; j++)
+                {
+                    _boardGraphicString += BoardSurface[i][j] + (j < BoardSurface[i].Length - 1 ? " " : "");
+                }
+                _boardGraphicString += i < BoardSurface.Length - 1 ? "\n" : "";
+            }
+            Console.WriteLine(_boardGraphicString);
+        }
+
+
+        private void InitializeBoardSurface()
+        {
+            BoardSurface = new string[_height][];
+
+            for (int i = 0; i < BoardSurface.Length; i++)
+            {
+                BoardSurface[i] = Enumerable.Repeat("[ ]", _width).ToArray();
+            }
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Battleships/Board Tests/BoardUnitTests.cs
@@ -0,0 +1,79 @@
+﻿using Battleships;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Board_Tests
+{
+    [TestClass]
+    public class BoardUnitTests
+    {
+        [TestMethod]
+        public void BoardTest2x3()
+        {
+            // Arrange
+            Board testBoard = new Board(2, 3);
+            string[][] expectedBoard = new string[2][];
+
+            expectedBoard[0] = new string[] { "[ ]", "[ ]", "[ ]" };
+            expectedBoard[1] = new string[] { "[ ]", "[ ]", "[ ]" };
+
+            // Act
+
+            // Assert
+            for (int i = 0; i < expectedBoard.Length; i++)
+            {
+                for (int j = 0; j < expectedBoard[0].Length; j++)
+                {
+                    Assert.AreEqual(expectedBoard[i][j], testBoard.BoardSurface[i][j]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void BoardTest3x2()
+        {
+            Board testBoard = new Board(3, 2);
+            string[][] expectedBoard = new string[3][];
+
+            expectedBoard[0] = new string[] { "[ ]", "[ ]" };
+            expectedBoard[1] = new string[] { "[ ]", "[ ]" };
+            expectedBoard[2] = new string[] { "[ ]", "[ ]" };
+
+            // Act
+
+            // Assert
+            for (int i = 0; i < expectedBoard.Length; i++)
+            {
+                for (int j = 0; j < expectedBoard[0].Length; j++)
+                {
+                    Assert.AreEqual(expectedBoard[i][j], testBoard.BoardSurface[i][j]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void PrintTest2x2()
+        {
+            Board testBoard = new Board(2, 2);
+            string expectedBoardGraphicString = "[ ] [ ]\n[ ] [ ]";
+
+            testBoard.Print();
+
+            Assert.AreEqual(expectedBoardGraphicString, testBoard.BoardGraphicString);
+        }
+
+        [TestMethod]
+        public void PrintTest5x5()
+        {
+            Board testBoard = new Board(5, 5);
+            string expectedBoardGraphicString = string.Concat(Enumerable.Repeat("[ ] [ ] [ ] [ ] [ ]\n", 5));
+            expectedBoardGraphicString = expectedBoardGraphicString.Substring(0, expectedBoardGraphicString.Length - 1);
+
+
+            testBoard.Print();
+
+            Assert.AreEqual(expectedBoardGraphicString, testBoard.BoardGraphicString);
+        }
+    }
+}
